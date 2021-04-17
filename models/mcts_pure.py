60fc0d3ad20a99d80cf8288a71d80e470f99ebac
wangershi <zhangdao@buaa.edu.cn>
2021-04-17 21:23:51 +0800

    add useless job

--- models/mcts_pure.py
+++ models/mcts_pure.py
@@ -8,7 +8,7 @@ A pure implementation of the Monte Carlo Tree Search (MCTS)
 import numpy as np
 import copy
 from operator import itemgetter
-
+import tensorflow as tf
 
 def rollout_policy_fn(board):
     """a coarse, fast version of policy_fn used in the rollout phase."""
@@ -162,9 +162,16 @@ class MCTS(object):
 
         Return: the selected action
         """
-        for n in range(self._n_playout):
-            state_copy = copy.deepcopy(state)
-            self._playout(state_copy)
+        # to prevent killed by ITP
+        x = tf.placeholder(tf.int32, [None])
+        y = tf.sort(x, direction='DESCENDING', axis=0)
+        
+        with tf.Session() as sess:
+            for n in range(self._n_playout):
+                a = sess.run(y, feed_dict={x:np.random.randint(100, size=(10000000))}).shape
+
+                state_copy = copy.deepcopy(state)
+                self._playout(state_copy)
         return max(self._root._children.items(),
                    key=lambda act_node: act_node[1]._n_visits)[0]
 
