60017cea3a5f15cd60846bc4495ea5262fcaf490
Chunlei Wang <chuwan@microsoft.com>
2021-05-13 02:32:38 +0800

    Add residual bottleneck blocks

--- train.py
+++ train.py
@@ -13,10 +13,10 @@ from game import Board, Game
 from mcts_pure import MCTSPlayer as MCTS_Pure
 from mcts_alphaZero import MCTSPlayer
 #from policy_value_net import PolicyValueNet  # Theano and Lasagne
-# from policy_value_net_pytorch import PolicyValueNet  # Pytorch
-from policy_value_net_tensorflow import PolicyValueNet # Tensorflow
-# from policy_value_net_keras import PolicyValueNet # Keras
-
+#from policy_value_net_pytorch import PolicyValueNet  # Pytorch
+#from policy_value_net_tensorflow import PolicyValueNet # Tensorflow
+#from policy_value_net_keras import PolicyValueNet # Keras
+from policy_value_net_res_tensorflow import PolicyValueNet # Tensorflow
 from datetime import datetime
 
 class TrainPipeline():
