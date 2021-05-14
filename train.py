ecb1aa9dce5d086e56b91c6aa1ff8e552a0379ae
Chunlei Wang <chuwan@microsoft.com>
2021-05-15 01:24:00 +0800

    Update board to 9,9,5

--- train.py
+++ train.py
@@ -22,9 +22,9 @@ from datetime import datetime
 class TrainPipeline():
     def __init__(self, init_model=None):
         # params of the board and the game
-        self.board_width = 6
-        self.board_height = 6
-        self.n_in_row = 4
+        self.board_width = 9
+        self.board_height = 9
+        self.n_in_row = 5
         self.board = Board(width=self.board_width,
                            height=self.board_height,
                            n_in_row=self.n_in_row)
